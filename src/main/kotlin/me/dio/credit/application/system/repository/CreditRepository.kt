c6c8a62153dc20383aad3a8ca30d5e84096869b2
origin <https://github.com/jeromario/desafio_poo.git>
2023-10-27 22:26:56 -0300

    Classes de Modelo

---
+++ src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
@@ -0,0 +1,7 @@
+package me.dio.credit.application.system.repository
+
+import me.dio.credit.application.system.entity.Credit
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CreditRepository: JpaRepository<Credit,Long> {
+}
